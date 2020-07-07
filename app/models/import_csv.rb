0603a4ee84120973e67e07f6a2c363514e5e8452
n.isobe <ganmodoki7.43dari@gmail.com>
2020-07-07 21:39:19 +0900

    first commit

---
+++ app/models/import_csv.rb
@@ -0,0 +1,21 @@
+class ImportCsv < ApplicationRecord
+
+  def self.import(path)
+    list = []
+    CSV.foreach(path, headers: true) do |row|
+      list << {
+        name: row["name"],
+        age: row["age"],
+        address: row["address"]
+      }
+    end
+    puts "インポート処理を開始"
+    begin
+      User.create!(list)
+      puts "インポート完了！"
+    rescue ActiveModel::UnknownAttributeError => invalid
+      puts "インポート失敗:UnknownAttributeError"
+    end
+  end
+
+end
